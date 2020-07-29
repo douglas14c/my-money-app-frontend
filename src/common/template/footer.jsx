176dc127e1fd2e39461216469ff91c5d26b7864f
Douglas Mateus <douglas.mateus@fcamara.com.br>
2020-07-28 21:40:50 -0300

    development of dashboard and minor components

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2017{" "}
+      <a href="https://github.com/douglas14c" target="_blank">
+        {" "}
+        Github
+      </a>
+    </strong>
+  </footer>
+);
---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ContentHeader from "../common/template/ContentHeader";
+import Content from "../common/template/Content";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <ContentHeader title="Dashboard" small="Versão 1.0.0" />
+      <Content>Dashboard</Content>
+    </div>
+  );
+}
